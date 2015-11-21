60bbdf87d480211b45861f9a85f183e29bd21761
Mike Pagel <mike@mpagel.de>
2015-11-21 14:43:45 +0100

    Access rights on root folder.

--- scripts/psnapshot.sh
+++ scripts/psnapshot.sh
@@ -7,5 +7,7 @@ python3 -m virtualenv psenv
 source psenv/bin/activate
 pip install --upgrade https://github.com/moltob/psnapshot/archive/master.zip
 
+chmod ug=rwX,o=rX /volume1/NetBackup
+
 mkdir flight7
 psnapshot -l DEBUG /volume1/NetBackup/flight7 flight7
